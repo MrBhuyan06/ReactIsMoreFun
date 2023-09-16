a91eb26a1cc085f1f1a2dcbcc84ffd3d854ee640
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-16 14:17:14 +0530

    added the useMemo Use Case

--- youtube/src/components/Demo.js
+++ youtube/src/components/Demo.js
@@ -1,26 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
+import { fact, findPrime } from "../config/helper.js";
 
 const Demo = () => {
   const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
 
-  useEffect(() => {
-    console.log("log");
-  }, [number]);
-  console.log("render");
+  //   const value = fact(number);
+  const prime = findPrime(number);
+
   return (
     <div>
-      <div className="h-96 w-96 bg-slate-200 mt-10 p-2">
+      <div
+        className={`h-96 w-96 mt-10 bg-slate-100 p-2 ${
+          dark && "bg-black text-white"
+        }    `}
+      >
+        <div>
+          <button
+            className=" bg-red-100  rounded-md  mb-2 px-4"
+            onClick={() => {
+              setDark(!dark);
+            }}
+          >
+            DarkTheme
+          </button>
+        </div>
         <input
           onChange={(e) => {
             setNumber(e.target.value);
           }}
+          value={number}
           type="number"
-          className="border-2-red-100 rounded-md"
+          className="border-2-red-100 rounded-md text-green-300"
         />
         <button className="px-4 ml-2 rounded-md bg-green-300">cal</button>
         <div>
-          <span className="font-semibold">Res{number}</span>
+          <span className="font-semibold">Res{prime}</span>
         </div>
       </div>
     </div>
