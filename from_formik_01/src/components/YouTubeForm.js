91bea96e9acb0b5e9ee92986bbb1d934e27da2a7
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-25 10:02:45 +0530

    done with the state management

--- from_formik_01/src/components/YouTubeForm.js
+++ from_formik_01/src/components/YouTubeForm.js
@@ -1,16 +1,46 @@
 import React from "react";
-import { formik } from "formik";
+import { formik, useFormik } from "formik";
 const YouTubeForm = () => {
+  //useFormik hook setup
+  const formik = useFormik({
+    initialValues: {
+      name: "Abhishek",
+      email: "",
+      Channel: "",
+    },
+  });
+  //this hook returb the method properity for functionality
+  //   console.log(formik);
+  // formik.value is trigger on formikHandler attached with event
+  // console.log(formik.values);
+
   return (
     <div>
       <label htmlFor="name">Name</label>
-      <input type="text" name="name" id="name" />
+      <input
+        type="text"
+        name="name"
+        id="name"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
       <br />
       <label htmlFor="email">Email</label>
-      <input type="email" name="email" id="email" />
+      <input
+        type="email"
+        name="email"
+        id="email"
+        onChange={formik.handleChange}
+        // value={}
+      />
       <br />
       <label htmlFor="channel">Channel</label>
-      <input type="text" name="channel" id="channel" />
+      <input
+        type="text"
+        name="channel"
+        id="channel"
+        onChange={formik.handleChange}
+      />
       <br />
       <button type="submit">Submit</button>
     </div>
