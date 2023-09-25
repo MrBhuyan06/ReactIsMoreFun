d9addef14fa723923bbd97d3d325206b52cc67fd
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-25 09:50:59 +0530

    added the ui

--- from_formik_01/src/components/YouTubeForm.js
+++ from_formik_01/src/components/YouTubeForm.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { formik } from "formik";
 const YouTubeForm = () => {
-  return <div>YouTubeForm</div>;
+  return (
+    <div>
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" id="name" />
+      <br />
+      <label htmlFor="email">Email</label>
+      <input type="email" name="email" id="email" />
+      <br />
+      <label htmlFor="channel">Channel</label>
+      <input type="text" name="channel" id="channel" />
+      <br />
+      <button type="submit">Submit</button>
+    </div>
+  );
 };
 
 export default YouTubeForm;
