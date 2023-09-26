f27b278b2496d546fb4fd452bca4610ba54581dd
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-26 11:35:25 +0530

    added the Formik Components

--- from_formik_01/src/components/NewYouTubeForm.js
+++ from_formik_01/src/components/NewYouTubeForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { formik, useFormik } from "formik";
+import { Formik, Form, useFormik, Field, ErrorMessage } from "formik";
 
 import {
   validate,
@@ -8,58 +8,34 @@ import {
   validationSchema,
 } from "../config/helper.js";
 const NewYouTubeForm = () => {
-  //useFormik hook setup
-  const formik = useFormik({
-    initialValues,
-    validationSchema,
-
-    onSubmit,
-  });
-  //this hook returb the method properity for functionality
-  //   console.log(formik);
-  // formik.value is trigger on formikHandler attached with event
-  console.log(formik.values);
-  // console.log(formik.errors);
-  console.log(formik.touched);
-
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        {...formik.getFieldProps("name")}
-      />
-      {formik.touched.name && formik.errors.name ? (
-        <div style={{ color: "red" }}>{formik.errors.name}</div>
-      ) : null}
-      <br />
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        {...formik.getFieldProps("email")}
-        // value={}
-      />
-      {formik.touched.email && formik.errors.email ? (
-        <div style={{ color: "red" }}>{formik.errors.email}</div>
-      ) : null}
-      <br />
-      <label htmlFor="channel">Channel</label>
-      <input
-        type="text"
-        name="channel"
-        id="channel"
-        {...formik.getFieldProps("channek")}
-      />
-      {formik.touched.channel && formik.errors.channel ? (
-        <div style={{ color: "red" }}>{formik.errors.channel}</div>
-      ) : null}
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onsubmit}
+      validationSchema={validationSchema}
+    >
+      <Form>
+        <label htmlFor="name">Name</label>
+        <Field type="text" name="name" id="name" />
+        <ErrorMessage />
+        <br />
+        <label htmlFor="email">Email</label>
+        <Field
+          type="email"
+          name="email"
+          id="email"
+
+          // value={}
+        />
+        <ErrorMessage />
+        <br />
+        <label htmlFor="channel">Channel</label>
+        <Field type="text" name="channel" id="channel" />
+        <ErrorMessage />
+        <br />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
 
