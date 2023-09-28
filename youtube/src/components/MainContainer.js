f0f37cba8a414a63ae6bd006bc9730f9b109db96
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-28 23:03:40 +0530

    edit the main container

--- youtube/src/components/MainContainer.js
+++ youtube/src/components/MainContainer.js
@@ -1,35 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ButtonList from "./ButtonList.js";
-import { BUTTONNAME } from "../contant/common.js";
-import { YOUTUBE_VIDEO_API } from "../contant/common.js";
-import VideoCard from "./VideoCard.js";
+import VideoContainer from "./VideoContainer.js";
+
 const MainContainer = () => {
-  const [videoList, setVideoList] = useState([]);
-  useEffect(() => {
-    getAllVideo();
-  }, []);
-  async function getAllVideo() {
-    const stramData = await fetch(YOUTUBE_VIDEO_API);
-    const data = await stramData.json();
-
-    setVideoList(data.items);
-  }
-  if (videoList.length === 0) {
-    console.log(videoList);
-    return;
-  }
-
   return (
-    <div className="col-span-10">
-      <div className="flex ">
-        {BUTTONNAME.map((btn) => {
-          return <ButtonList key={btn.id} {...btn} />;
-        })}
-      </div>
-      <div className="mt-4 flex flex-wrap justify-center gap-2">
-        {videoList.map((video) => {
-          return <VideoCard key={video.id} info={video} />;
-        })}
+    <div className="w-5/5 border-2">
+      <div>
+        <ButtonList />
+        <VideoContainer />
       </div>
     </div>
   );
