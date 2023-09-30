271959b18a9f853fd5c3a0a59d6325dd7bddd7db
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-30 21:40:14 +0530

    added the video config

--- youtube/src/components/VideoCard.js
+++ youtube/src/components/VideoCard.js
@@ -1,22 +1,28 @@
 import React from "react";
 
-const VideoCard = ({ info }) => {
-  const { snippet, statistics } = info;
-  const { channelTitle, thumbnails, title } = snippet;
+const VideoCard = ({ data }) => {
+  // console.log(data);
+  const { snippet, statistics } = data;
   // console.log(snippet, statistics);
+
   return (
-    <div className="w-[200px] h-[240px] border-2 p-2 shadow-lg ">
+    <div className="w-[230px] h-[240px] p-2  border-2 rounded-md">
       <div>
-        <img className="" src={thumbnails?.medium.url} alt="" />
+        <img
+          src={snippet.thumbnails.medium.url}
+          alt=""
+          className="rounded-md"
+        />
       </div>
-      <div>
-        <h2 className="font-bold">{channelTitle}</h2>
-        <h2 className="underline">{title.split("|")[0]}</h2>
+      <div className="p-2">
+        <ul className="flex flex-col">
+          <l1 className="font-bold">{snippet.channelTitle}</l1>
+          <l1 className="font-semibold underline">
+            {snippet.title.split("|")[0]}
+          </l1>
+          <l1 className="text-blue-400">{statistics.viewCount} View</l1>
+        </ul>
       </div>
-      <h3 className="bg-gray-300 p-2 rounded-lg">
-        {" "}
-        view {statistics.viewCount}
-      </h3>
     </div>
   );
 };
