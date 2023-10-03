5296f155a3893e84cbffbe087adf08cd6ccb9985
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-10-03 18:41:44 +0530

    refactore the code

---
+++ youtube/src/config/config.js
@@ -0,0 +1,92 @@
+const API_KEY = "AIzaSyDQy93BfrmmqLu4P8ouvWpiJjQVshghIxM";
+export const YOUTUBE_VIDEO_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyDQy93BfrmmqLu4P8ouvWpiJjQVshghIxM";
+
+export const SEARCH_SUGGESTION_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+// "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+
+export const COMMENTS_DATA = [
+  {
+    name: "Abhishek",
+    text: "Nice Movie",
+    replies: [],
+  },
+  {
+    name: "Abhishek",
+    text: "What Does the hero NAme",
+    replies: [
+      {
+        name: "Smriti",
+        text: "Dont Know",
+        replies: [
+          {
+            name: "GUDU",
+            text: "Name SRRK",
+            replies: [
+              {
+                name: "Abhihske",
+                text: "Thank You",
+                replies: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "ABHISHEK BHUYAN",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [
+          {
+            name: "ABHISHEK BHUYAN",
+            text: "Lorem ipsum dolor sit amet, consectetur adip",
+            replies: [
+              {
+                name: "ABHISHEK BHUYAN",
+                text: "Lorem ipsum dolor sit amet, consectetur adip",
+                replies: [
+                  {
+                    name: "ABHISHEK BHUYAN",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [
+                      {
+                        name: "ABHISHEK BHUYAN",
+                        text: "Lorem ipsum dolor sit amet, consectetur adip",
+                        replies: [],
+                      },
+                    ],
+                  },
+                  {
+                    name: "ABHISHEK BHUYAN",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "ABHISHEK BHUYAN",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "ABHISHEK BHUYAN",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "ABHISHEK BHUYAN",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "ABHISHEK BHUYAN",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+];
